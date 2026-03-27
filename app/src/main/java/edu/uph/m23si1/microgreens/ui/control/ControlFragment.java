75da06b6797ba46b3f9a904f8a46b5b5f25f841f
NatashaAngelie <natasha.angelie25@gmail.com>
2026-03-27 17:23:44 +0700

    Part 3 - Control

--- app/src/main/java/edu/uph/m23si1/microgreens/ui/control/ControlFragment.java
+++ app/src/main/java/edu/uph/m23si1/microgreens/ui/control/ControlFragment.java
@@ -1,66 +1,81 @@
 package edu.uph.m23si1.microgreens.ui.control;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.uph.m23si1.microgreens.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ControlFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ControlFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button ledOn, ledOff;
+    Button pumpOn, pumpOff;
+    Button fanOn, fanOff;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ControlFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ControlFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ControlFragment newInstance(String param1, String param2) {
-        ControlFragment fragment = new ControlFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    DatabaseReference controlRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_control, container, false);
+
+        ledOn = view.findViewById(R.id.ledOn);
+        ledOff = view.findViewById(R.id.ledOff);
+
+        pumpOn = view.findViewById(R.id.pumpOn);
+        pumpOff = view.findViewById(R.id.pumpOff);
+
+        fanOn = view.findViewById(R.id.fanOn);
+        fanOff = view.findViewById(R.id.fanOff);
+
+        controlRef = FirebaseDatabase.getInstance().getReference("microgreens/control");
+
+        setupButtons();
+        readFirebase();
+
+        return view;
+    }
+
+    private void setupButtons() {
+
+        ledOn.setOnClickListener(v -> controlRef.child("led").setValue(true));
+        ledOff.setOnClickListener(v -> controlRef.child("led").setValue(false));
+
+        pumpOn.setOnClickListener(v -> controlRef.child("pump").setValue(true));
+        pumpOff.setOnClickListener(v -> controlRef.child("pump").setValue(false));
+
+        fanOn.setOnClickListener(v -> controlRef.child("fan").setValue(true));
+        fanOff.setOnClickListener(v -> controlRef.child("fan").setValue(false));
+    }
+
+    private void readFirebase() {
+
+        controlRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                Boolean led = snapshot.child("led").getValue(Boolean.class);
+                Boolean pump = snapshot.child("pump").getValue(Boolean.class);
+                Boolean fan = snapshot.child("fan").getValue(Boolean.class);
+
+                // nanti kalau mau tampilkan status bisa disini
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 }
